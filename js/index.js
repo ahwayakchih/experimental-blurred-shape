c8f8b0abab6497975bfe48f6f56c3bdeb1d90db4
wlodi <wlodek.grzegorz@gmail.com>
2018-03-16 17:42:03 +0100

    Added smooth shape for blur.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,29 @@
 /* global BlurShape */
 
-(function () {
-	const content = document.getElementById('main');
-	const blur = new BlurShape(content);
-	console.log(blur);
+const content = document.getElementById('main');
+const blur = new BlurShape(content);
 
+function normalBlur() {
+   blur.shapeUrl = BLUR_SHAPES.CIRCLE;
+};
+
+function smoothBlur() {
+    blur.shapeUrl = BLUR_SHAPES.CIRCLE_SMOOTH;
+};
+
+function appendMouseMove() {
 	content.addEventListener('mousemove', e => {
-		const rect = content.getBoundingClientRect();
-		const x = e.clientX - rect.left;
-		const y = e.clientY - rect.top;
-		const dpr = window.devicePixelRatio;
+        const rect = content.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        const dpr = window.devicePixelRatio;
 
-		blur.x = ((x - (blur.width / 2 / dpr)) * dpr).toFixed(2);
-		blur.y = ((y - (blur.height / 2 / dpr)) * dpr).toFixed(2);
-	});
+        blur.x = ((x - (blur.width / 2 / dpr)) * dpr).toFixed(2);
+        blur.y = ((y - (blur.height / 2 / dpr)) * dpr).toFixed(2);
+    });
+}
+
+(function() {
+    console.log(blur);
+	appendMouseMove();
 })();
